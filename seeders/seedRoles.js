eddcd46864407df9c81245d40b9b2b86edeb94c2
Mirza-Azeem11 <30124618@students.southwales.ac.uk>
2025-09-03 13:15:28 -0700

    Remove nurse receptionist from seed roles
    remove nurse receptionist from seed roles

--- seeders/seedRoles.js
+++ seeders/seedRoles.js
@@ -1,3 +1,4 @@
+// idar ya roles add ry ga 
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
@@ -49,32 +50,7 @@ module.exports = {
         created_at: new Date(),
         updated_at: new Date()
       },
-      {
-        name: 'Nurse',
-        description: 'Nursing staff with patient care access',
-        color: 'bg-purple-500',
-        permissions: JSON.stringify([
-          'patient_management',
-          'appointment_management',
-          'messaging'
-        ]),
-        is_system_role: true,
-        created_at: new Date(),
-        updated_at: new Date()
-      },
-      {
-        name: 'Receptionist',
-        description: 'Front desk staff with appointment and patient registration access',
-        color: 'bg-yellow-500',
-        permissions: JSON.stringify([
-          'appointment_management',
-          'patient_registration',
-          'messaging'
-        ]),
-        is_system_role: true,
-        created_at: new Date(),
-        updated_at: new Date()
-      }
+     
     ];
 
     await queryInterface.bulkInsert('roles', roles, {
